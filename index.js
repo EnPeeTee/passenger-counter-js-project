969393916a7f9e2186a816c8872b91b6a473196b
Patrick T <enteepee2@gmail.com>
2022-09-19 15:36:28 +0800

    init

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+// let currentValue = 0
+
+// document.getElementById("increment-btn").addEventListener("click", () => {
+//     currentValue = parseInt(document.getElementById("count").innerText) + 1
+//     document.getElementById("count").innerText = currentValue
+//     console.log(currentValue)
+// })
+
+// document.getElementById("save-btn").addEventListener("click", () => {
+//     document.getElementById("current-increments").innerText += ` ${currentValue} - `
+//     document.getElementById("count").innerText = 0
+//     currentValue = 0
+//     console.log(currentValue)
+// })
+
 let currentValue = 0
 
+function add() {
+    currentValue += 1
+    const result = document.getElementById("count").textContent = currentValue
+    console.log(result)
+}
 
-
-document.getElementById("increment-btn").addEventListener("click", () => {
-    currentValue = parseInt(document.getElementById("count").innerText) + 1
-    document.getElementById("count").innerText = currentValue
-    console.log(currentValue)
-})
-
-document.getElementById("save-btn").addEventListener("click", () => {
-    document.getElementById("current-value").innerText += ` ${currentValue} - `
-    document.getElementById("count").innerText = 0
+function save() {
+    document.getElementById('current-increments').textContent += ` ${currentValue} - `
+    document.getElementById("count").textContent = 0
     currentValue = 0
     console.log(currentValue)
-})
+}
